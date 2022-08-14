0322bcfabe7584be39370b106666aa3d208ecdc9
Nur Islam <nurislam0333@gmail.com>
2022-08-14 22:03:04 +0200

    Add CharacterFromName func to get a character from its name

--- game/character.go
+++ game/character.go
@@ -4,3 +4,18 @@ type Character interface {
 	Beats(Character) string
 	Name() string
 }
+
+func CharacterFromName(name string) Character {
+	switch name {
+	case "rock":
+		return Rock{}
+	case "paper":
+		return Paper{}
+	case "scissors":
+		return Scissors{}
+	case "well":
+		return Well{}
+	}
+
+	return nil
+}
