a62d17a6704e15b0e2027dcf6ca201b271a98312
Nur Islam <nurislam0333@gmail.com>
2022-08-14 19:37:14 +0200

    Add func for random generator

--- utils/random_number.go
+++ utils/random_number.go
@@ -1,1 +1,13 @@
 package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+// GenerateRandomNumberWithLimit returns random int following [0,limit)
+func GenerateRandomNumberWithLimit(limit int) int {
+	rand.Seed(time.Now().UnixNano())
+	randNumber := rand.Intn(limit)
+	return randNumber
+}
